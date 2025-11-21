cfc4666902dd8eb0d064f8b7eae4d0d34806924f
fairlight1337 <jan.winkler.84@gmail.com>
2025-11-21 08:53:16 -0800

    Add CLI11 and brief README

---
+++ README.md
@@ -0,0 +1,14 @@
+Checkout:
+
+git clone <...>
+git submodule update --init --recursive
+
+Build:
+
+mkdir build
+cmake ..
+make
+
+Format:
+
+make format
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include <homeshell/Homeshell.hpp>
+#include <CLI/CLI.hpp>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
     using namespace homeshell;
 
+    CLI::App app{"Homeshell - A modern shell for home automation"};
+    
+    // Add version flag
+    bool show_version = false;
+    app.add_flag("-v,--version", show_version, "Show version information");
+    
+    // Add configuration file option
+    std::string config_file = "";
+    app.add_option("-c,--config", config_file, "Configuration file path");
+    
+    // Add verbose flag
+    bool verbose = false;
+    app.add_flag("--verbose", verbose, "Enable verbose output");
+    
+    // Parse arguments
+    CLI11_PARSE(app, argc, argv);
+    
+    // Handle version flag
+    if (show_version)
+    {
+        Version version;
+        std::cout << "Homeshell v" << version.getVersionString() << std::endl;
+        return 0;
+    }
+    
     Homeshell homeshell;
-
     return homeshell.run().code;
 }
